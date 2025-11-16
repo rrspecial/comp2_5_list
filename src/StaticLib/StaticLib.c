e70e6df2e0dfc53aab53bcc90ab156e3a8d06876
rrspecial <rrspkouya@gmail.com>
2025-11-16 17:51:40 +0900

    ビルドを通しました

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -21,13 +21,18 @@ void initialize_list(list* l)
 // 先頭にデータを追加
 void push_front(list* l, node* p)
 {
-	// ToDo: ここにコードを追加
+	if (l == NULL || p == NULL) return;
+	p->pNext = l->header;
+	l->header = p;
 }
 
 // pの次のノードを削除
 void remove_next(list* l, node* p)
 {
-	// ToDo: ここにコードを追加
+	if (l == NULL || p == NULL|| p->pNext == NULL) return;
+			node* del = p->pNext;
+			p->pNext = del->pNext;
+			del->pNext = NULL;
 }
 
 
